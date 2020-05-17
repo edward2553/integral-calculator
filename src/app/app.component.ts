7bca1f622fc59262ebc453649c3dfb8ee50c0200
edward moron quintana <edwaramor9@gmail.com>
2020-05-17 15:35:26 -0500

    se tiene el front end listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Expression } from './models/Expression';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-basica';
+
+  expresionString : Expression  = new Expression();
+
+  addElement( element : string ){
+
+    this.expresionString.expression += element;
+
+  }
+
+  // employeeArray: Employee[] = [
+  //   {id: 1 , name: 'Edward' , country: 'Colombia'},
+  //   {id: 2 , name: 'Claudia' , country: 'Venezuela'},
+  //   {id: 3 , name: 'Marian' , country: 'Costa Rica'},
+  // ];
+
+  // selectedEmployee : Employee = new Employee();
+
+  // addOrEdit(){
+  //   this.selectedEmployee.id = this.employeeArray.length + 1;
+  //   this.employeeArray.push(this.selectedEmployee)
+  //   this.selectedEmployee = new Employee();
+  // }
+
 }
